d3ff58c3a525698315325f57a8a2579fff0945c9
Marianna1903 <84705083+Marianna1903@users.noreply.github.com>
2021-09-09 17:45:11 +0400

    Add files via upload

---
+++ js2.js
@@ -0,0 +1,89 @@
+// var a=-3;b=-5;
+// var b=Math.abs(a+b);
+// console.log(b);
+
+
+// var x=(Math.random()*100+10);
+// console.log(x);
+
+// var p = prompt("age");
+// if(p<18){
+// 	alert("mutqn argelvac e");
+// }else{
+// 	document.write("Hajoxutyamb mutq eq gorcel");
+// }
+
+// var p = +prompt("number");
+// switch(p){
+// 	case 0:
+// 	case 1:
+// 	case 11:
+// 	alert("Winter");
+// 	break;
+// 	case 2:
+// 	case 3:
+// 	case 4:
+// 	alert("Spring");
+// 	break;
+// 	case 5:
+// 	case 6:
+// 	case 7:
+// 	alert("Summer");
+// 	break;
+// 	case 8:
+// 	case 9:
+// 	case 10:
+// 	alert("Autumn");
+// 	break;
+// 	default:
+// 	alert("Not found");
+// }
+
+
+// var color = "Red";
+// if(color=="Red"){
+// 	alert("Stop!");
+// }else if(color=="Green"){
+// 	alert("Go!");
+// }else{
+// 	alert("Wait!");
+// }
+
+
+// var age=+prompt("Age");
+// if(age<=17){
+// 	alert("You have to study.");
+// }else if(age>=17 && age<=56){
+// 	alert("You have to work.");
+// }else{
+// 	alert("You are a pensioner.")
+// }
+
+
+
+// var a=3,b=8,c=-5,d=9;
+// if(a>b && a>c && a>d){
+// 	console.log(a);
+// }else if(b>a && b>c && b>d){
+// 	console.log(b);
+// }else if(c>a && c>b && c>d){
+// 	console.log(c);
+// }else{
+// 	console.log(d);
+// }
+
+
+// var a = prompt("Nermucir mterqy");
+// switch(a){
+// 	case "macun":
+// 	alert("macuny arje "+200+"dram");
+// 	break;
+// 	case "hac":
+// 	alert("hacy arje "+150+"dram");
+// 	break;
+// 	case "panir":
+// 	alert("paniry arje "+450+"dram");
+// 	break;
+// 	default:
+// 	alert("Arka che");
+// }
